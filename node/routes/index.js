1663bbac15a54a8481f6c6162cb291b4105fb061
amoralesc12 <alejandramorales.2001@unitec.edu>
2024-01-27 15:57:12 -0600

    Commit que se tuvo que realizar el dia jueves en clase con el ejemplo de uso de axios

---
+++ node/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+/* GET home page. */
+router.get('/', async (req, res, next) => {
+  try {
+    const { data: countries } = await axios.get('https://restcountries.com/v2/all');
+    const randomIndex = Math.floor(Math.random() * countries.length);
+    const randomCountry = countries[randomIndex];
+    res.render('index', { title: 'Welcome to my branch', randomCountry });
+    console.log('Random Country Data:', randomCountry);
+  } catch (error) {
+    console.error('Axios Error:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
