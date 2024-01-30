8d4fb5e6fe97a5dd8a7235c807b0cc0c831c46d1
amoralesc12 <alejandramorales.2001@unitec.edu>
2024-01-30 00:15:06 -0600

    commit sobre lo hecho en clase, sale que la api key es invalida

--- node/routes/index.js
+++ node/routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
+require('dotenv').config();
 
-/* GET home page. */
+/*
+// GET home page. 
 router.get('/', async (req, res, next) => {
   try {
     const { data: countries } = await axios.get('https://restcountries.com/v2/all');
@@ -15,6 +17,19 @@ router.get('/', async (req, res, next) => {
     console.error('Axios Error:', error.message);
     res.status(500).json({ error: 'Internal Server Error' });
   }
-});
+});*/
 
+/*GET WEATHER */
+router.get('/', async (req, res,next) => {
+  try {
+    const weatherApiKey = process.env.WEATHER_API;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${req.query.city}&appid=${weatherApiKey}`;
+    const { data: weatherData } = await axios.get(weatherUrl);
+    res.json(weatherData);
+    console.log('Weather Data:', weatherData);
+  } catch (error) {
+    console.error('Axios Error:', error.message);
+    res.status(500).json({ error: 'Error al obtener datos del clima' });
+  }
+});
 module.exports = router;
