3c73efd1254775e751621572e6d45bcac5950a5e
amoralesc12 <alejandramorales.2001@unitec.edu>
2024-02-10 00:16:34 -0600

    limpiando todas las branches, y solo dejando lo requerido, cada dia de clase (solo confirmar conexion con la API KEY)

--- node/routes/index.js
+++ node/routes/index.js
@@ -4,32 +4,19 @@ const axios = require('axios');
 
 require('dotenv').config();
 
-/*
-// GET home page. 
-router.get('/', async (req, res, next) => {
-  try {
-    const { data: countries } = await axios.get('https://restcountries.com/v2/all');
-    const randomIndex = Math.floor(Math.random() * countries.length);
-    const randomCountry = countries[randomIndex];
-    res.render('index', { title: 'Welcome to my branch', randomCountry });
-    console.log('Random Country Data:', randomCountry);
-  } catch (error) {
-    console.error('Axios Error:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});*/
 
 /*GET WEATHER */
-router.get('/', async (req, res,next) => {
+
+
+router.get("/", async function (req, res, next) {
   try {
-    const weatherApiKey = process.env.WEATHER_API;
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${req.query.city}&appid=${weatherApiKey}`;
-    const { data: weatherData } = await axios.get(weatherUrl);
-    res.json(weatherData);
-    console.log('Weather Data:', weatherData);
+    const response = await axios.get(
+      "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + process.env.WEATHER_API
+    );
+    res.send(response.data);
   } catch (error) {
-    console.error('Axios Error:', error.message);
-    res.status(500).json({ error: 'Error al obtener datos del clima' });
+    res.status(500).send("Server Error");
   }
 });
+
 module.exports = router;
